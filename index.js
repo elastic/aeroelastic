8057fceb37aae9758c923eb72f31cf6e20dd6912
Robert Monfera <monfera.robert@gmail.com>
2018-04-07 11:55:23 +0200

    click

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 (() => {
 
-  const scenegraph = React.createElement('div', {}, 'Hello, Worrrld!')
+  const scenegraph = React.createElement('div', {
+    className: 'circle',
+    onClick: () => console.log('clicked'),
+    style: {
+      width: 100,
+      height: 100
+    }
+  })
   const root = document.getElementById('root')
   ReactDOM.render(scenegraph, root)
 
