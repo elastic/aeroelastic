0a2c1094dd217aafe765ccf8bef48119315980f7
Robert Monfera <monfera.robert@gmail.com>
2018-04-07 13:29:54 +0200

    cursor moves

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 (() => {
 
-  const metaCursor = React.createElement('div', {
-    className: 'circle',
-    style: {
-      width: 100,
-      height: 100
-    }
-  })
+  const transactions = {
+    cursorPositions: []
+  }
 
-  const substrate = React.createElement('div', {
-    id: 'root',
-    onClick: () => console.log('clicked')
-  },
-    [metaCursor]
-  )
+  const cursor = {
+    x: 200,
+    y: 200
+  }
 
   const root = document.body
 
-  ReactDOM.render(substrate, root)
+  const render = () => {
+
+    const metaCursor = React.createElement('div', {
+      className: 'circle',
+      style: {
+        width: 100,
+        height: 100,
+        transform: `translate(${cursor.x}px, ${cursor.y}px)`
+      }
+    })
+
+    const substrate = React.createElement('div', {
+        id: 'root',
+        onMouseMove: event => {
+          cursor.x = event.clientX
+          cursor.y = event.clientY
+          console.log('clicked')
+          render()
+        }
+      },
+      [metaCursor]
+    )
+
+    ReactDOM.render(substrate, root)
+  }
+
+  render()
 
 })()
